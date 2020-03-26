f1cc19c86800d64bb47a3eb37227e7629e2d77ee
Grzesiek <data.grzegorz@gmail.com>
2020-03-26 10:41:21 +0100

    commite 1041

---
+++ src/pages/Kontakt.vue
@@ -0,0 +1,24 @@
+<template>
+  <Layout>
+    <div class="container mx-auto px-6 sm:px-0">
+      <article class="mb-20">
+        <header class="mt-10 pt-10 mb-10 text-center">
+          <h1 class="text-5xl font-bold">Kontakt</h1>
+        </header>
+        <section class="post-full-content">
+          <div class="post-content text-2xl font-serif font-hairline text-gray-700 no-image">
+            <p>Jesteśmy otwarci na nawiązanie współpracy reklamowej, patronackiej oraz redeakcyjnej. Ponieważ chcemy by nasz portal medyczny był rzetelny, możemy zgodzić się na reklamowanie produktu czy usługi jedynie zgodnych z naszsymi przekonaniami i wiedzą.</p> <p>Kontakt: <a href="mailito:kontakt@managermedyczny.pl"> kontakt@managermedyczny.pl</a></p>
+          </div>
+        </section>
+      </article>
+    </div>
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: "Kontakt"
+  }
+};
+</script>
